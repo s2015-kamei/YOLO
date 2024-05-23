f55b1bbc97b9a612d4fd3e9557be4348f4eddf3a
s2015-kamei <s2015@oita.kosen-ac.jp>
2024-05-23 21:00:42 +0900

    temp

--- realtime.py
+++ realtime.py
@@ -3,28 +3,33 @@ import cv2
 from PIL import Image
 import numpy as np
 import pyrealsense2 as rs
+import socket
 
-# カメラの設定
-conf = rs.config()
-# RGB
-conf.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-# 距離
-conf.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-pipe = rs.pipeline()
-profile = pipe.start(conf)
+class RS_YOLO:
+    
+    def __init__(self):
+        # カメラの設定
+        self.conf = rs.config()
+        # RGB
+        self.conf.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        # 距離
+        self.conf.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-# Alignオブジェクト生成
-align_to = rs.stream.color
-align = rs.align(align_to)
+        self.pipe = rs.pipeline()
+        self.profile = self.pipe.start(self.conf)
 
-model = YOLO("runs/detect/train16/weights/best.pt")
+        # Alignオブジェクト生成
+        self.align_to = rs.stream.color
+        self.align = rs.align(self.align_to)
 
-try:
-    while True:
+        self.model = YOLO("runs/detect/train16/weights/best.pt")
+
+    def get_distance(self):
+        
         # フレーム待ち
-        frames = pipe.wait_for_frames()
-        aligned_frames = align.process(frames)
+        frames = self.pipe.wait_for_frames()
+        aligned_frames = self.align.process(frames)
 
         #RGB
         RGB_frame = aligned_frames.get_color_frame()
@@ -35,10 +40,11 @@ try:
         depth_image = np.asanyarray(depth_frame.get_data())
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.08), cv2.COLORMAP_JET)
 
-        results = model.predict(RGB_image, show=False, verbose=False) 
+        results = self.model.predict(RGB_image, show=False, verbose=False) 
 
         results_number = len(results)
         results_images = []
+        results_data = []
         for result in results:
             #print(result.boxes.xyxy.size())
             if(result.boxes.xyxy.shape[0] == 0):
@@ -73,15 +79,26 @@ try:
 
                 # 表示
                 results_images.append(image)
+                results_data.append((x1, x2, y1, y2, depth_average))
 
         for i, result_image in enumerate(results_images):
             cv2.imshow(f"result{i}", result_image)
         cv2.waitKey(1)
+        return results_data
 
-finally:
-    pipe.stop()
-    cv2.destroyAllWindows()
+        # finally:
+        #     self.pipe.stop()
+        #     cv2.destroyAllWindows()
+        #     return None
         
 
-
-    
+if __name__ == "__main__":
+    serv_address = ('127.0.0.1', 1357)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    yolo = RS_YOLO()
+    while True:
+        data = yolo.get_distance()
+        if data is not None:
+            send_len = sock.sendto(data, serv_address)
+        
+        
